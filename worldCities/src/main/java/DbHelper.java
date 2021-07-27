5d23390825039894607606540bb988f853301dcf
Mustafa Genc <mustafagenc20@gmail.com>
2021-07-27 11:21:09 +0200

    Swing exercises

---
+++ worldCities/src/main/java/DbHelper.java
@@ -0,0 +1,7 @@
+
+public class DbHelper {
+    private String userName="root";
+    private String password="Aslanlar_528";
+    private String dbUrl="jdbc:mysql://localhost:3306/world?useSSL=false&serverTimezone=UTC";
+    
+}
---
+++ worldCities/target/site/apidocs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"<Unnamed>","l":"City"},{"p":"<Unnamed>","l":"DbHelper"},{"p":"<Unnamed>","l":"demo"}]
